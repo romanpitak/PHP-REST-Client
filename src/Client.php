ed1f571fecafb63ee752d470367e871b8a908cad
Roman Piták <roman@pitak.net>
2014-03-05 12:36:05 +0100

    Replaced magic methods and overloading with actual methods.

--- src/Client.php
+++ src/Client.php
@@ -11,9 +11,6 @@ namespace RestClient;
 /**
  * Class Client
  * @package RestClient
- * @method mixed getOption(string $key, mixed $default = null)
- * @method void setOption(string $key, mixed $value)
- * @method array setConfig(array $config);
  */
 class Client {
 
@@ -69,11 +66,42 @@ class Client {
 	}
 
 	/*
-	 * ========== Overloading ==========
+	 * ========== Getters ==========
 	 */
 
-	public function __call($name, $arguments) {
-		return call_user_func_array(array($this->request, $name), $arguments);
+	/**
+	 * Get the configuration key of the client (request).
+	 *
+	 * @param string $key
+	 * @param mixed $default Default value to return if no value is set.
+	 * @return mixed Config value.
+	 */
+	public function getOption($key, $default = null) {
+		return $this->request->getOption($key, $default);
+	}
+
+	/*
+	 * ========== Setters ==========
+	 */
+
+	/**
+	 * Set configuration parameter.
+	 *
+	 * @param string $key Configuration key
+	 * @param string $value Value
+	 */
+	public function setOption($key, $value) {
+		$this->request->setOption($key, $value);
+	}
+
+	/**
+	 * Merge the current configuration array with the $config array provided.
+	 *
+	 * @param array $config Configuration array to be merged with the current configuration.
+	 * @return array Current configuration array after the merge.
+	 */
+	public function setConfig($config) {
+		return $this->request->setConfig($config);
 	}
 
 }
