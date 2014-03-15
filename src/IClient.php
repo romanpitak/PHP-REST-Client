2bb841d8cc15f1ef1a24b1cab67f828c0069d863
Roman Piták <roman.pitak@hokosoft.cz>
2014-03-15 22:51:56 +0100

    interfaces added

---
+++ src/IClient.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * IClient
+ *
+ * @author Roman Piták <roman@pitak.net>
+ * @package romanpitak/php-rest-client
+ *
+ */
+
+namespace RestClient;
+
+/**
+ * Interface IClient
+ */
+interface IClient {
+
+	const BASE_URL_KEY = IRequest::BASE_URL_KEY;
+	const CURL_OPTIONS_KEY = IRequest::CURL_OPTIONS_KEY;
+	const HEADERS_KEY = IRequest::HEADERS_KEY;
+	const METHOD_KEY = IRequest::METHOD_KEY;
+	const PASSWORD_KEY = IRequest::PASSWORD_KEY;
+	const DATA_KEY = IRequest::DATA_KEY;
+	const USER_AGENT_KEY = IRequest::USER_AGENT_KEY;
+	const USERNAME_KEY = IRequest::USERNAME_KEY;
+
+	/**
+	 * @param string $url
+	 * @param string $method
+	 * @param string $data
+	 * @param array $headers
+	 * @return IRequest
+	 */
+	public function newRequest($url, $method = 'GET', $data = null, $headers = array());
+
+	/**
+	 * Get the configuration key of the client (request).
+	 *
+	 * @param string $key
+	 * @param mixed $default Default value to return if no value is set.
+	 * @return mixed Config value.
+	 */
+	public function getOption($key, $default = null);
+
+	/**
+	 * Set configuration parameter.
+	 *
+	 * @param string $key Configuration key
+	 * @param string $value Value
+	 */
+	public function setOption($key, $value);
+
+	/**
+	 * Merge the current configuration array with the $config array provided.
+	 *
+	 * @param array $config Configuration array to be merged with the current configuration.
+	 * @return array Current configuration array after the merge.
+	 */
+	public function setConfig($config);
+
+} 
---
+++ src/IResponse.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * IResponse
+ *
+ * @author Roman Piták <roman@pitak.net>
+ * @package romanpitak/php-rest-client
+ *
+ */
+
+namespace RestClient;
+
+/**
+ * Interface IResponse
+ */
+interface IResponse {
+
+	/**
+	 * Return the raw curl_exec() output
+	 *
+	 * @return string
+	 */
+	public function getReturnedTransfer();
+
+	/**
+	 * Return the response body
+	 *
+	 * @return string
+	 */
+	public function getParsedResponse();
+
+	/**
+	 * (object)curl_getinfo()
+	 *
+	 * @return object
+	 */
+	public function getInfo();
+
+	/**
+	 * Return a string containing the last error for the current session
+	 *
+	 * @return string
+	 */
+	public function getCurlError();
+
+	/**
+	 * Return the last error number
+	 *
+	 * @return int
+	 */
+	public function getCurlErrorNumber();
+
+} 
